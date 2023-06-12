b3875f8f28a22dedf99dac04607dc81031b7b780
najohnson11936 <najohnson11936@outlook.com>
2023-06-11 21:57:42 -0400

    added javadoc

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 	
+	/**
+	 * Handle the index endpoint
+	 * @return
+	 */
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
 	
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String start() {
 		return "index";
