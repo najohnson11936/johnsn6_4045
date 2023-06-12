11aa107bb4957582bf2f33aa24db236e664f043a
najohnson11936 <najohnson11936@outlook.com>
2023-06-11 21:53:47 -0400

    checked in other essential files

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
-
+	
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
 	
+	@RequestMapping("/")
+	public String start() {
+		return "index";
+	}
 }
